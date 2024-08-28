792e0ae273be2dd6f2c033cf2da894c1655235aa
kevin <kipronok234@gmail.com>
2024-08-28 12:46:13 +0300

    looping.py-p3

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
+    for i in range(10, 0, -1):
+        print(i)
+        print('Happy New Year!')
     # code goes here!
     pass
 
-def square_integers(int_list):
+def square_integers(integers):
+    return[i ** 2 for i in integers]
     # code goes here!
     pass
 
 def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
     # code goes here!
     pass
